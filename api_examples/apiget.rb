7357f63df2a0fe58804378ed850939382725c194
Tim Spencer <tspencer@cloudpassage.com>
2013-02-27 14:45:21 -0800

    added code from wstearns that puts in https_proxy support

--- api_examples/apiget.rb
+++ api_examples/apiget.rb
@@ -14,7 +14,18 @@ require 'oauth2'
 require 'rest-client'
 require 'json'
 
+#To accomodate a proxy, we need to handle both RestClient with the
+#following one-time statement, and also as a :proxy parameter to the
+#oauth2 call below.
+my_proxy = nil
+if ENV['https_proxy'].to_s.length > 0
+  my_proxy = ENV['https_proxy']
+  RestClient.proxy = my_proxy
+  $stderr.puts "Using proxy: #{RestClient.proxy}"
+end
+
 client = OAuth2::Client.new(clientid, clientsecret,
+        :connection_opts => { :proxy => my_proxy },
         :site => "https://#{host}",
         :token_url => '/oauth/access_token'
 )
